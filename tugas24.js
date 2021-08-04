db4a7c594a3a4cfdb06cf723cc17613f62515500
Ariq Fikri Tamamy <ariqrick@gmail.com>
2021-08-04 21:19:42 +0700

    Tugas 24 Javascript

---
+++ tugas24.js
@@ -0,0 +1,9 @@
+function urutkanData(){
+  var data = [32,33,16,40,9,7,11,2,44,1,66];
+  console.log("Sebelumnya :",data.toString())
+
+  data = data.sort()
+  console.log("Ascending :",data.toString())
+  console.log("Descending :",data.reverse().toString())
+}
+urutkanData()
